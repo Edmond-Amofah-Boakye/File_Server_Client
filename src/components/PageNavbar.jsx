c1f2bc23be7465535d571491379f017abfa286ae
Edmond Amofah Boakye <edmondboakye1622@gmail.com>
2023-07-03 00:01:05 +0000

    update

--- src/components/PageNavbar.jsx
+++ src/components/PageNavbar.jsx
@@ -2,7 +2,6 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logout from './Helpers/Logout'
-import { Link } from 'react-router-dom'
 
 function PageNavbar() {
   const isLoggedin = localStorage.getItem("role");
@@ -20,23 +19,23 @@ function PageNavbar() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="m-auto">
-            <Link to='/home'>Home</Link>
-            <Link to='/home'>About</Link>
-            <Link to='/home'>Feeds</Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/feeds">Feeds</Nav.Link>
           </Nav>
           {!isLoggedin && (
             <Nav>
-              <Link to="/signin" className="sign">
+              <Nav.Link eventKey={2} href="/signin" className="sign">
                 <button className="signup-btn">Sign in</button>
-              </Link>
+              </Nav.Link>
             </Nav>
           )}
 
           {isLoggedin && (
             <Nav>
-              <Link className="sign">
+              <Nav.Link eventKey={2} className="sign">
                 <button className="signup-btn bg-danger" onClick={()=>logout()}>Sign out</button>
-              </Link>
+              </Nav.Link>
             </Nav>
           )}
         </Navbar.Collapse>
