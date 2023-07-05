8d6d53273200ea43622ea04b872da3ddc1001d66
Edmond-Amofah-Boakye <80597593+Edmond-Amofah-Boakye@users.noreply.github.com>
2023-07-05 01:16:47 +0000

    Update Server.js

--- src/components/Helpers/Server.js
+++ src/components/Helpers/Server.js
@@ -1,3 +1,4 @@
-const server = "https://fileserverapp-api.onrender.com/api/v1";
+// const server = "https://fileserverapp-api.onrender.com/api/v1";
+const server = "https://dark-gold-duck-wear.cyclic.app;
 
 export default server;
